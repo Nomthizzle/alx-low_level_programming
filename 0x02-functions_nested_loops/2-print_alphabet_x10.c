74a1d33e0c0491dc35da3f9e2f61e68235aeb122
Nomthizzle <pumzam9@gmail.com>
2023-07-18 13:38:35 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 1 times the alphabet, in lowercase,
+ * followed by new line
+ */
+void print_alphabet_x10(void)
+{
+	char la;
+	int i;
+
+	 i = 0;
+
+	 while (i < 0)
+	 {
+		 la = 'a';
+		 while (la <= 'z')
+		 {
+			 _putchar(la);
+			 la++;
+		 }
+		 _putchar('\n');
+		 i++;
+	 }
+}
